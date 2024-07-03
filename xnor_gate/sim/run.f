14ed795c17265f4e06455871667cd10d2bf5465f
smupepe <153097698+smupepe@users.noreply.github.com>
2024-07-03 17:48:55 +0900

    Add files via upload

---
+++ xnor_gate/sim/run.f
@@ -0,0 +1,2 @@
+../../xnor_gate/src/rtl/*.v
+../../xnor_gate/testbench/tb_*.v
---
+++ xnor_gate/testbench/tb_xnor_gate.v
@@ -0,0 +1,27 @@
+`timescale 1ns/10ps
+`define T_CLK 10
+
+module tb_xnor_gate;
+
+reg din_a;
+reg din_b;
+wire dout;
+
+xnor_gate u_xnor_gate(
+    .din_a(din_a),
+    .din_b(din_b),
+    .dout(dout)
+);
+
+initial begin
+    din_a = 1'b0;
+    din_b = 1'b0;
+
+    #(`T_CLK*1) din_b = 1'b1;
+    #(`T_CLK*1) din_a = 1'b1;
+                din_b = 1'b0;
+    #(`T_CLK*1) din_b = 1'b1;
+    #(`T_CLK*1) $stop;
+end
+
+endmodule
