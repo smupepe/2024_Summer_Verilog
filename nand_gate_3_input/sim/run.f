7ee49aaa4043a393dc1ccfb022a653731c6363a1
smupepe <153097698+smupepe@users.noreply.github.com>
2024-07-03 17:46:09 +0900

    Add files via upload

---
+++ nand_gate_3_input/sim/run.f
@@ -0,0 +1,2 @@
+../../nand_gate_3_input/src/rtl/*.v
+../../nand_gate_3_input/testbench/tb_*.v
---
+++ nand_gate_3_input/src/rtl/nand_gate_3_input.v
@@ -0,0 +1,15 @@
+`timescale 1ns/10ps
+
+module nand_gate_3_input(
+    din_a,
+    din_b,
+    din_c,
+    dout
+);
+
+input din_a, din_b, din_c;
+output dout;
+
+assign dout = ~(din_a & din_b & din_c);
+
+endmodule
